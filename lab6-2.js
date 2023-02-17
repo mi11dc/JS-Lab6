77ce330228d59a66ad08033ef02c12bc608dc1de
mi11dc <mi1996.dhaval@gmail.com>
2023-02-17 12:07:33 -0500

    Lab 6 Files Added

---
+++ lab6-2.js
@@ -0,0 +1,46 @@
+﻿//#### LAB 6 - DOM MANIPULATION ####
+//PART 2: MYSTERY BOX
+
+//LISTEN FOR load EVENT
+window.onload = pageReady;
+
+//'WRAPPER' FUNCTION FOR DOM LOGIC
+function pageReady() {
+
+	var userInput;
+	var userInputPopupTxt = "Do you want to see something?";
+	//GET DOM ELEMENTS WE'LL NEED
+	var mysteryBoxElement = document.getElementById("mysteryBox");
+	var buttonBoxElement = document.getElementById("buttonBox");
+
+	//====CREATE THE FUNCTIONS WE'LL NEED====
+	mysteryBoxElement.onmouseover = createPopup;
+	//FUNCTION TO ASK USER
+	function createPopup() {
+		userInput = confirm(userInputPopupTxt);
+		if (userInput === true) {
+			mysteryBoxElement.style.display = "none";
+			buttonBoxElement.style.display = "block";
+
+			buttonBoxElement.onclick = clickButtonBox;
+			buttonBoxElement.style.cursor = "pointer";
+		}
+	}
+
+	//FUNCTION TO CHANGE buttonBox
+	function clickButtonBox() {
+		buttonBoxElement.style.width = "600px";
+		buttonBoxElement.style.backgroundColor = "orange";
+		buttonBoxElement.style.cursor = "";
+
+		buttonBoxElement.innerHTML = "<h2>SURPRISE!!</h2>";
+
+		// var h2Element = buttonBoxElement.getElementsByTagName("h2")[0];
+		// h2Element.innerText = "SURPRISE!!";
+	}
+
+	//SETUP LISTENERS
+
+
+//END onload FUNCTION
+}
